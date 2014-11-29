440c11b3212ee7667910860ae50a7fe77488bd0f
Asylzhan <asylzhan13@gmail.com>
2014-11-29 23:48:23 +0600

    Initial Commit

---
+++ DNA into RNA/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DNA into RNA
+//
+//  Created by asyl13 on 29.11.14.
+//  Copyright (c) 2014 Asylzhan Salimkhan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
